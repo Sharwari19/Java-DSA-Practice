7ab0170d0c203a3b4b5dbbe5f05c62e06311cbe4
Sharwari <sharwarigole@gmail.com>
2025-02-06 20:02:49 +0530

    Added EvenOdd solution in Basic

---
+++ Basic/EvenOdd.java
@@ -0,0 +1,22 @@
+// Write a program to print whether a number is even or odd, also take input from the user.
+
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the number to check");
+
+        int input = scan.nextInt();
+
+        if(input % 2 == 0) 
+        {
+            System.out.println(input + " is Even");
+        }
+        else 
+        {
+            System.out.println(input + " is Odd");
+        }
+    }
+}
